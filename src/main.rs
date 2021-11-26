77593ff431044b90d1680489022ff49e6abe51a4
mark-summerfield <mark@qtrac.eu>
2021-11-26 08:47:11 +0000

    renaming

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::panic;
 
 fn main() {
     panic::set_hook(Box::new(|info| {
+        dialog::message_title("Error — Gravitate");
         if let Some(sender) = info.payload().downcast_ref::<&str>() {
             dialog::message(center().0 - 200, center().1 - 100, sender);
         } else {
@@ -23,26 +24,26 @@ fn main() {
             );
         }
     }));
-    // let config = read_config(); // TODO read gravitate.ini
+    // let query = read_query(); // TODO read gravitate.ini
     // command line args override .ini
-    handle_commandline(); // TODO pass config
-                          // TODO Pass config to application object
+    handle_commandline(); // TODO pass query
+                          // TODO Pass query to application object
     let mut app = Application::new();
     app.run();
 }
 
 fn handle_commandline() {
-    // TODO config passed in
+    // TODO query passed in
     let mut scale: f32 = 1.0; // TODO delete
     for arg in std::env::args().skip(1) {
         if arg.starts_with("--scale=") {
             scale = num::clamp(
-                // TODO scale → config.scale
+                // TODO scale → query.scale
                 arg.get(8..).unwrap().parse::<f32>().unwrap_or(scale),
                 0.5,
                 2.5,
             );
         }
     }
-    app::set_screen_scale(0, scale); // TODO scale → config.scale
+    app::set_screen_scale(0, scale); // TODO scale → query.scale
 }
